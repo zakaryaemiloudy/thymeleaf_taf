b4ca4fe4e1e1bb0be81da258a3685ce7d4993977
zakaryaemiloudy <zakarayemiloudy6@gmail.com>
2025-12-20 22:47:55 +0100

    3 methodes de spring securité

--- src/main/java/com/example/taf_thymeleaf/TafThymeleafApplication.java
+++ src/main/java/com/example/taf_thymeleaf/TafThymeleafApplication.java
@@ -7,8 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 @SpringBootApplication //(exclude = {SecurityAutoConfiguration.class})
 public class TafThymeleafApplication {
@@ -42,6 +45,35 @@ public class TafThymeleafApplication {
 
         };
     }
+
+
+
+
+    @Bean
+    CommandLineRunner jbdcauthentification(JdbcUserDetailsManager jdbcUserDetailsManager, PasswordEncoder passwordEncoder) {
+        return args -> {
+
+
+            UserDetails u1 = User.withUsername("user1").password(passwordEncoder.encode("user")).roles("USER").build();
+            UserDetails u2 = User.withUsername("user2").password(passwordEncoder.encode("user")).roles("USER").build();
+
+            UserDetails a1 = User.withUsername("admin").password(passwordEncoder.encode("admin")).roles("USER","ADMIN").build();
+
+            jdbcUserDetailsManager.createUser(u1);
+            jdbcUserDetailsManager.createUser(u2);
+            jdbcUserDetailsManager.createUser(a1);
+
+        };
+    }
+
+
+
+
+
+
+
+
+
 
     @Bean
     PasswordEncoder passwordEncoder(){
--- src/main/java/com/example/taf_thymeleaf/security/SecurityConfig.java
+++ src/main/java/com/example/taf_thymeleaf/security/SecurityConfig.java
@@ -6,16 +6,30 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+
+
     @Bean
+    public JdbcUserDetailsManager  JdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+
+    }
+
+
+
+    //@Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder){
         return new InMemoryUserDetailsManager(
                 User.withUsername("user1").password(passwordEncoder.encode("user")).roles("USER").build(),
@@ -25,17 +39,17 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http){
-//        return http.formLogin(Fl->Fl.loginPage("/login").permitAll());
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .formLogin(fl->fl.loginPage("/login").defaultSuccessUrl("/").permitAll())
                 .authorizeHttpRequests(ar-> ar.requestMatchers("/admin/**").hasRole("ADMIN"))
                 .authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER"))
                 .authorizeHttpRequests(ar->ar.requestMatchers("/public/**").permitAll())
+                .authorizeHttpRequests(ar->ar.requestMatchers("/h2-console/**").permitAll())
                 .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
                 .exceptionHandling(eh -> eh.accessDeniedPage("/accessDenied"))
-
-
+//                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .build();
     }
 
